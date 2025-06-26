ec9d77880788e0597143b8ce3a0d13951e432654
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-06-26 17:52:48 -0500

    fix: use timestamp for created_at and updated_at fields

--- migrations/1_create_users_table.up.sql
+++ migrations/1_create_users_table.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users
 (
     id         CHARACTER(36) PRIMARY KEY,
-    name       TEXT    NOT NULL,
+    name       TEXT      NOT NULL,
     birth      DATE,
     email      TEXT UNIQUE,
     location   TEXT,
-    created_at DATE    NOT NULL,
-    updated_at DATE    NOT NULL,
-    active     BOOLEAN NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    active     BOOLEAN   NOT NULL
 );
