efbfd4b133121e7fad39ed9265cc48fd28242122
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-04 18:09:03 -0500

    test: fix update func signature

--- infrastructure/server/handlers/update_test.go
+++ infrastructure/server/handlers/update_test.go
@@ -9,21 +9,27 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"users/domain/entities"
 	"users/infrastructure/dependencies"
 )
 
 type UpdateMock struct {
-	execute func(context.Context, string, map[string]interface{}) error
+	execute func(context.Context, string, map[string]interface{}) (*entities.User, error)
+	answer  *entities.User
 	err     error
 }
 
-func NewUpdateMock(err error) *UpdateMock {
+func NewUpdateMock(answer *entities.User, err error) *UpdateMock {
 	mock := &UpdateMock{
-		err: err,
+		answer: answer,
+		err:    err,
 	}
 
-	mock.execute = func(ctx context.Context, id string, fields map[string]interface{}) error {
-		return err
+	mock.execute = func(ctx context.Context, id string, fields map[string]interface{}) (*entities.User, error) {
+		if err != nil {
+			return nil, err
+		}
+		return answer, nil
 	}
 
 	return mock
@@ -39,35 +45,35 @@ func TestUpdate(t *testing.T) {
 	}{
 		{
 			name:         "on OK execution",
-			update:       NewUpdateMock(nil),
+			update:       NewUpdateMock(&entities.User{Name: "test"}, nil),
 			body:         bytes.NewReader([]byte(`{"name":"test"}`)),
-			expectedCode: http.StatusNoContent,
-			expectedBody: "",
+			expectedCode: http.StatusOK,
+			expectedBody: "{\"data\":{\"id\":\"\",\"name\":\"test\",\"birth\":\"\",\"email\":\"\",\"location\":null,\"created_at\":\"0001-01-01 00:00:00\",\"updated_at\":\"0001-01-01 00:00:00\",\"active\":false}}",
 		},
 		{
 			name:         "on invalid type",
-			update:       NewUpdateMock(nil),
+			update:       NewUpdateMock(nil, nil),
 			body:         bytes.NewReader([]byte(`{"name":1}`)),
 			expectedCode: http.StatusBadRequest,
 			expectedBody: "{\"errors\":\"json: cannot unmarshal number into Go struct field UpdateUser.name of type string\"}",
 		},
 		{
 			name:         "on nil payload",
-			update:       NewUpdateMock(nil),
+			update:       NewUpdateMock(nil, nil),
 			body:         nil,
 			expectedCode: http.StatusBadRequest,
 			expectedBody: "{\"errors\":\"cannot read nil body\"}",
 		},
 		{
 			name:         "on invalid birth",
-			update:       NewUpdateMock(nil),
+			update:       NewUpdateMock(nil, nil),
 			body:         bytes.NewReader([]byte(`{"name":"test","birth":"23/09/92"}`)),
 			expectedCode: http.StatusBadRequest,
 			expectedBody: "{\"errors\":\"error while parsing 'birth' field from \\\"23/09/92\\\": parsing time \\\"23/09/92\\\" as \\\"02/01/2006\\\": cannot parse \\\"92\\\" as \\\"2006\\\"\"}",
 		},
 		{
 			name:         "on save repository error",
-			update:       NewUpdateMock(errors.New("an error occurred")),
+			update:       NewUpdateMock(nil, errors.New("an error occurred")),
 			body:         bytes.NewReader([]byte(`{"name":"test"}`)),
 			expectedCode: http.StatusInternalServerError,
 			expectedBody: "{\"errors\":\"an error occurred\"}",
